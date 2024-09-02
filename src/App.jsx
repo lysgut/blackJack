dd4b6a99ef3816741082afb0c51c4eca5d8e0662
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2024-09-02 10:27:09 -0300

    proyecto incial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import './App.css'; // Asegúrate de importar el archivo CSS
+
+const App = () => {
+ 
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default App;
